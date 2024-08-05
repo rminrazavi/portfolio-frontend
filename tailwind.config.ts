10f69ffcacc0ec5666959bbaf9f63beeed8e2f75
Armin Razavi <arminrazavi79@gmail.com>
2024-08-05 19:17:45 +0330

    ✨ Feat: Add a footer

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from "tailwindcss";
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
   content: [
@@ -13,8 +16,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "var(--neutral-700)",
+        secondary: "var(--neutral-500)",
+      },
     },
   },
-  plugins: [],
-};
+  plugins: [require("@tailwindcss/typography"), addVariablesForColors],
+} satisfies Config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
